5abd2a6f9020ff95ab0216bfe595bf4ce71b8cc9
Kyle Lai <kl655@cornell.edu>
2020-06-16 16:52:45 -0400

    Added about page

--- budget-tracker-frontend/src/static/About.jsx
+++ budget-tracker-frontend/src/static/About.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import './../styles/about.css';
 
 class About extends React.Component {
   render() {
     return (
-      <div>This is the about page</div>
+      <div className='container'>
+        <section id='graphic'>
+        </section>
+        <section id='text'>
+          <h1 id='heading'>Never forget a subscription again.</h1>
+          <p id='paragraph'>Simplify the way you manage your subscriptions.</p>
+          <p>Input monthly subscriptions.</p>
+          <p>Enter a budget.</p>
+          <p>Display renewal dates.</p>
+          <p>Keep track of monthly expenses.</p>
+
+        </section>
+
+      </div>
     )
 
 
--- budget-tracker-frontend/src/static/SinglePageApp.jsx
+++ budget-tracker-frontend/src/static/SinglePageApp.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from './Navbar.jsx';
 import Home from './Home.jsx';
 import './../styles/App.css';
 import {
