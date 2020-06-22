ab48d4253f26e44cc824160a5af1822d6df10408
Henry Li <hl738@cornell.edu>
2020-06-22 14:51:19 -0400

    created page and js script to create new user

--- api/api/models.py
+++ api/api/models.py
@@ -1,6 +1,6 @@
 from api import db
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     first_name = db.Column(db.String, nullable = False)
     last_name = db.Column(db.String, nullable = False)
