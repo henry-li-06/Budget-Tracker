ac254710961caa9c1010ba38106940ae69cc9a61
Henry Li <hl738@cornell.edu>
2020-06-30 11:41:49 -0400

    Created utils module for authenticating user and validating new user information

--- api/api/views.py
+++ api/api/views.py
@@ -1,33 +1,17 @@
 from api import app, db, bcrypt
 from .models import User, Expense, RefreshToken
+from api.utils.auth import *
+from api.utils import validate_new_user
 
 from flask import make_response, request
-from functools import wraps
 from uuid import uuid4
-import jwt
 from datetime import datetime, timedelta
-
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        access_token = request.cookies.get('x-access-token')
-        if(access_token):
-            try:
-                data = jwt.decode(access_token, app.config['SECRET_KEY'])
-                current_user = User.query.filter_by(public_id = data['public_id']).first()
-            except:
-                return { 'message' : 'Access token is invalid!' }, 401
-        else:
-            return { 'message' : 'Access token is missing!' }, 401
-        
-        return f(current_user, *args, **kwargs)
-    return decorated
 
 # *** ENDPOINTS ***
 
 @app.route('/user/new', methods = ['POST'])
-def new_user():
-    data = request.get_json()
+@validate_new_user
+def new_user(data):
 
     first_name = data['first_name']
     last_name = data['last_name']
@@ -35,18 +19,18 @@ def new_user():
     password = data['password']
     email = data['email']
     
-    if(True): # TODO: Check for valid login information
-        hashed_password = bcrypt.generate_password_hash(password)
-        new_user = User(first_name = first_name, last_name = last_name, username = username, \
-            password = hashed_password, email = email, public_id = str(uuid4()))
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-            return { 'message' : 'New user created!' }, 201
-        except:
-            pass
     
-    return { 'message' : 'There was an issue creating a new user!' }
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_user = User(first_name = first_name, last_name = last_name, username = username, \
+        password = hashed_password, email = email, public_id = str(uuid4()))
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        return { 'message' : 'New user created!' }, 201
+     except:
+        return { 'message' : 'There was an issue creating a new user!' }
+
+    
 
 
 @app.route('/user/login', methods = ['POST'])
@@ -58,37 +42,19 @@ def login():
 
         if(user and bcrypt.check_password_hash(user.password, auth.password)):
 
-            access_token = jwt.encode({
-                'public_id' : user.public_id,
-                'exp' : datetime.utcnow() + timedelta(minutes = 30)
-            }, app.config['SECRET_KEY'])
+            access_token = generate_access_token(user.public_id)
+            refresh_token = generate_refresh_token(user.public_id)
 
-            refresh_token = jwt.encode({
-                'public_id' : user.public_id,
-                'exp' : datetime.utcnow() + timedelta(weeks = 2)
-            }, app.config['REFRESH_SECRET_KEY'])
+            if(not refresh_token):
+                return { 'message' : 'There was an issue loggin in!' }, 401
 
             res = make_response({ 'message' : 'User successfully logged in!' })
-            res.set_cookie('x-access-token', value = access_token.decode('UTF-8'), httponly = True, samesite = \
+            res.set_cookie('x-access-token', value = access_token, httponly = True, samesite = \
                 None, expires = datetime.utcnow() + timedelta(minutes = 30))
-            res.set_cookie('x-refresh-token', value = refresh_token.decode('UTF-8'), httponly = True, samesite = \
+            res.set_cookie('x-refresh-token', value = refresh_token, httponly = True, samesite = \
                 None, expires = datetime.utcnow() + timedelta(weeks = 2), path = '/user/login/refresh')
 
-            db_refresh_token = RefreshToken.query.filter_by(user_public_id = user.public_id).first()
-            if(db_refresh_token):
-                db_refresh_token.is_valid = True
-                db_refresh_token.expiration_date = datetime.utcnow() + timedelta(weeks = 2)
-                db.session.commit()
-            else:
-                try:
-                    db_refresh_token = RefreshToken(user_public_id = user.public_id, expiration_date = datetime.utcnow() \
-                        + timedelta(weeks = 2))
-                    db.session.add(db_refresh_token)
-                    db.session.commit()
-                except:
-                    return { 'message' : 'There was an issue logging in!' }, 401
-
-            return res
+            return res, 200
     
     return { 'message' : 'Invalid username or password!' }, 401
 
@@ -134,28 +100,20 @@ def new_expense(current_user):
 
 @app.route('/user/login/refresh', methods = ['GET'])
 def get_new_access_token():
-    refresh_token = request.cookies.get('x-refresh-token')
+    refresh_token = request.cookeis.get('x-refresh-token')
     if(refresh_token):
-        try:
-            data = jwt.decode(refresh_token, app.config['REFRESH_SECRET_KEY'])
-            user_public_id = data['public_id']
-            db_refresh_token = RefreshToken.query.filter_by(user_public_id = user_public_id).first()
-
-            if(db_refresh_token and db_refresh_token.is_valid and db_refresh_token.expiration_date > datetime.utcnow()):
-                new_access_token = jwt.encode({
-                    'public_id' : user_public_id,
-                    'exp' : datetime.utcnow() + timedelta(minutes = 30)
-                }, app.config['SECRET_KEY'])
-                res = make_response({ 'message' : 'New access token granted!' })
-                res.set_cookie('x-access-token', value = new_access_token.decode('UTF-8'), httponly = True, expires = \
+        user_public_id = verify_refresh_token(refresh_token)
+        if(user_public_id):
+            
+            new_access_token = generate_access_token(user_public_id)
+            res = make_response({ 'message' : 'New access token granted!' })
+            res.set_cookie('x-access-token', value = new_access_token, httponly = True, expires = \
                     datetime.utcnow() + timedelta(minutes = 30), samesite = None)
-                return res, 200
-        except:
-            pass
-
-        return { 'message' : 'Invalid refresh token!' }, 401
-
-    return { 'message' : 'Refresh token not found!' }, 401
+            return res, 200
+        else:
+            return { 'message' : 'Refresh token is invalid!' }, 401
+    
+    return { 'message' : 'No refresh token found!' }, 401
 
 
 @app.after_request
