b779f55a2d08baea916e06cd09bb68a36197aa6d
Kyle Lai <kl655@cornell.edu>
2020-08-11 14:57:30 -0400

    Added logo image

--- budget-tracker-frontend/src/static/SinglePageApp.jsx
+++ budget-tracker-frontend/src/static/SinglePageApp.jsx
@@ -10,6 +10,7 @@ import About from "./About.jsx";
 import Login from "./Login.jsx";
 import Signup from "./Signup.jsx"
 import BudgetLists from './BudgetLists.jsx';
+import img from './../images/budgettrackerimage.png'
 
 class SinglePageApp extends React.Component {
 
@@ -21,7 +22,7 @@ class SinglePageApp extends React.Component {
             <div className="container">
               <section id="navbar">
                 <ul id="list">
-                  <li id="heading">Budget Tracker</li>
+                  <li id="heading"><img src={img} alt='Budget Tracker' style={{ height: '40px' }} /> Budget Tracker</li>
                   <li className="link"><NavLink exact to='/'>Home</NavLink></li>
                   <li className="link"><NavLink to='/about'>About</NavLink></li>
                   <li className="link"><NavLink to='/login'>Log in</NavLink></li>
